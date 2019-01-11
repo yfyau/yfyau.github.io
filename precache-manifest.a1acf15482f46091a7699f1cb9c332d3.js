5ec40be9e9643e248fbce5efdb3997fde78a001d
Jason Yau <yfyau@connect.ust.hk>
2019-01-11 13:46:04 +0800

    Updates

---
+++ precache-manifest.a1acf15482f46091a7699f1cb9c332d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30ed6e33991aa7cd7b60",
+    "url": "/static/js/main.30ed6e33.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "30ed6e33991aa7cd7b60",
+    "url": "/static/css/main.f76ba120.chunk.css"
+  },
+  {
+    "revision": "de8a667fb1e1b27d38cf34d45670f9ff",
+    "url": "/index.html"
+  }
+];
