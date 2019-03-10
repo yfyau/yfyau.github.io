0a7dad3e089028596ddeb6b2af76d22b3b7c0a31
Jason Yau <yfyau@connect.ust.hk>
2019-03-11 01:28:20 +0800

    Updates

---
+++ precache-manifest.a425ed8386d12f7011f93a0eabde2319.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc16fb9fad364c0adea4fe0f88dc90bc",
+    "url": "/static/media/OrderEat.dc16fb9f.png"
+  },
+  {
+    "revision": "82e2ee62f8685ae100a1",
+    "url": "/static/css/main.db6d2dfd.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6fb63d64e4df0634f150",
+    "url": "/static/js/2.6fb63d64.chunk.js"
+  },
+  {
+    "revision": "ecc41bdb1187f8cef194feea8d1e464b",
+    "url": "/static/media/angle-up-solid.ecc41bdb.svg"
+  },
+  {
+    "revision": "8bad87734ac6c834243fa4eb96cc1fe1",
+    "url": "/static/media/angle-down-solid.8bad8773.svg"
+  },
+  {
+    "revision": "82e2ee62f8685ae100a1",
+    "url": "/static/js/main.82e2ee62.chunk.js"
+  },
+  {
+    "revision": "3f60245657cb7fdbb51146f995743a51",
+    "url": "/static/media/ITinHK.3f602456.png"
+  },
+  {
+    "revision": "40af6ec8f9d54aced72b6f0b6fec9b11",
+    "url": "/static/media/MoneyDrive.40af6ec8.png"
+  },
+  {
+    "revision": "ebd77fdf6593d844bd2a6303dbd6923a",
+    "url": "/static/media/Email.ebd77fdf.png"
+  },
+  {
+    "revision": "d09360dc49e425b65105f8c05a1ade70",
+    "url": "/static/media/Poly-Bee.d09360dc.svg"
+  },
+  {
+    "revision": "9acab2b6dc826b321d5e775f3a2ad880",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.318001f867d510eea15bfb99aa80bcd3.js"
+  "/precache-manifest.a425ed8386d12f7011f93a0eabde2319.js"
 );
 
 workbox.clientsClaim();
