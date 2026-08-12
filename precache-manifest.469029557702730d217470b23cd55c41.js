953aa2be27f13b080f695a4d659c0c7a5de0ca53
yfyau <jason.yfyau@gmail.com>
2026-08-12 15:29:00 -0400

    Updates

---
+++ precache-manifest.469029557702730d217470b23cd55c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1944b3cbf14032cc04b27b3da3924ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40ea22a49f64f4689da8",
+    "url": "/static/css/main.b7c8cfeb.chunk.css"
+  },
+  {
+    "revision": "c8842c60386a2ffd9052",
+    "url": "/static/js/2.41422347.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.41422347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ea22a49f64f4689da8",
+    "url": "/static/js/main.47de3fd1.chunk.js"
+  },
+  {
+    "revision": "ca86b32ec09a4ecac524",
+    "url": "/static/js/runtime-main.db47d54b.js"
+  }
+]);
